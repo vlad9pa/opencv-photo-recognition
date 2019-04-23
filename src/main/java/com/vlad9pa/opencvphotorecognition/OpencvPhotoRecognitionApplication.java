3c1d693c3a80d74f66c49092b8950bae127f8492
Vlad M <vlad@cryptanite.com>
2019-04-23 14:26:36 +0300

    init commit

---
+++ src/main/java/com/vlad9pa/opencvphotorecognition/OpencvPhotoRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.vlad9pa.opencvphotorecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencvPhotoRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencvPhotoRecognitionApplication.class, args);
+    }
+
+}
